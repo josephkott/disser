519360fe157399bcb65e1455ff1ea7e9b81a5edd
josephkott <gloriouslair@gmail.com>
2021-09-03 18:48:03 +0300

    move mex files to the repository

---
+++ calc/mex/Solver.h
@@ -0,0 +1,60 @@
+#ifndef SOLVER_H_
+#define SOLVER_H_
+
+#include <cstring>
+#include <iostream>
+#include <cmath>
+
+#include "Point.h"
+#include "Solution.h"
+#include "OdeProblem.h"
+
+namespace NR
+{
+
+//const double UPPER_BOUND = 10000000000000.0;
+const double UPPER_BOUND = 100000.0;
+	
+template <int dim>
+class Solver
+{
+public:
+	Solution<dim> Solve(OdeProblem<dim> OP, const int intCount)
+	{
+		Solution<dim> sol;
+		sol.nodesCount = intCount + 1;
+		sol.step = (OP.tspan[1] - OP.tspan[0]) / intCount;
+		sol.points = new Point<dim>[sol.nodesCount];
+		sol.isInfinit = false;
+		sol.points[0] = OP.Init;
+		sol.tspan = new double[2];
+		sol.tspan[0] = OP.tspan[0];
+		sol.tspan[1] = OP.tspan[1];
+
+		double t = sol.tspan[0];
+		double h = sol.step;
+		Point<dim> k[4];
+		for (int i = 0; i < intCount; i++)
+		{
+			k[0] = OP.f(t, sol.points[i]) * h;
+			k[1] = OP.f(t + 0.5 * h, sol.points[i] + k[0] * 0.5) * h;
+			k[2] = OP.f(t + 0.5 * h, sol.points[i] + k[1] * 0.5) * h;
+			k[3] = OP.f(t + h, sol.points[i] + k[2]) * h;
+
+			sol.points[i + 1] = sol.points[i] + (k[0] + k[1] * 2.0 + k[2] * 2.0 + k[3]) * (1.0 / 6.0);
+
+			if (sol.points[i + 1].abs() > UPPER_BOUND)
+			{
+				sol.isInfinit = true;
+				sol.tspan[1] = t;
+				sol.nodesCount = i;
+				return sol;
+			}
+			t += h;
+		}
+		return sol;
+	}
+};
+
+}
+#endif
---
+++ calc/mex/functions.h
@@ -0,0 +1,45 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include <cmath>
+
+#include "Point.h"
+using namespace NR;
+using namespace std;
+
+// NLS with piecewise constant nonlinearity with absence of linear potential.
+// Equation for nonlinear stationary modes:
+// $u_{xx} - \omega u + \sigma(x) u^3 = 0$,
+// where $\sigma(x) = -1, x \in [0, L_1)$ & $\sigma = +1, x \in [L_1, L_2)$,
+// $L = L_1 + L_2$ -- period of nonlinear potential.
+// Parameters: $[\omega L_1 L_2]$.
+Point<2> piecewise(double x, Point<2> u, double* param)
+{
+	Point<2> du;
+	double omega = param[0], L1 = param[1], L2 = param[2];
+    double L = L1 + L2;
+    
+    double n = floor(x / L);
+    double x0 = x - n * L;
+    double sigma = (x0 < L1 ? -1 : +1);
+    
+	du[0] = u[1];
+	du[1] = omega * u[0] - sigma * pow(u[0], 3);
+	return du;
+}
+
+// NLS with absent of linear potential and presence of nonlinear potential
+// Equation for nonlinear stationary modes:
+// $u_{xx} - {\omega} u + (\alpha + \cos{2x}) u^3 = 0$.
+// Parameters: $[\omega \alpha]$.
+Point<2> cosine(double x, Point<2> u, double* param)
+{
+	Point<2> du;
+	double omega = param[0], alpha = param[1]
+
+	du[0] = u[1];
+	du[1] = omega * u[0] - (alpha + cos(2 * x)) * pow(u[0], 3);
+	return du;
+}
+
+#endif
